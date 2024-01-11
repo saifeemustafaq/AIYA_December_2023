387e712dabe610464b6937a42e01ac3c05f8eec4
saifeemustafaq <saifeemustafaq@gmail.com>
2024-01-11 13:10:29 +0530

    username

--- .github/scripts/sync_script.py
+++ .github/scripts/sync_script.py
@@ -33,8 +33,8 @@ def update_repo2(latest_file_content, repo2_name, file_path_in_repo2):
     repo.git.checkout(new_branch)
 
     # Set Git config
-    repo.git.config('user.email', 'saifeemustafaq@gmail.com')
-    repo.git.config('user.name', 'saifeemustafaq')
+    repo.git.config('user.email', '41898282+github-actions[bot]@users.noreply.github.com')
+    repo.git.config('user.name', 'github-actions[bot]')
 
     # Update the file in repo2
     file_path = os.path.join(repo2_dir, file_path_in_repo2)
